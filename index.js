39c75382d8bebf102ddd151d057f705e30bab080
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-03-06 18:14:01 -0300

    criando request de times

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
-
+const http =  require('http');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({
-    message: "Deu certo cara"
+  var apiKey = "9748c352c8d847569d6b752d90f6ceaf";
+  var url = "http://api.football-data.org/v2/competitions/2001/teams";
+
+  var options = {
+    method: "GET",
+    headers: {
+      "X-Auth-Token": apiKey
+    }
+  };
+
+  var data = "";
+
+  let apiRequest = http.request(url, options, function(res) {
+    console.log("conectado");
+
+    res.on("data", chunk => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      response.end(JSON.stringify(data));
+    });
   });
+
+  apiRequest.end();
 });
 
-app.listen(3333);
+http.createServer(app).listen(3333);
